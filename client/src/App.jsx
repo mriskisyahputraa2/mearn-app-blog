2e1fefc7b019f1d2e9e4f099c1cfa1982962b6f2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-08-14 14:02:19 +0700

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { Button } from "flowbite-react";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Project from "./Pages/Projects/Project";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        {/* start header top */}
+        <Header />
+        {/* end header top */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/project" element={<Project />} />
+          <Route path="/projects" element={<Project />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/Pages/Projects/Project.jsx
+++ client/src/Pages/Projects/Project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return <div>Projects</div>;
 };
 
 export default Project;
