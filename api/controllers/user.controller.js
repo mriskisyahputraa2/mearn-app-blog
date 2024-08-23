d091cff7ad4604bda926265fffe7f43516b3cdb3
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-08-23 15:35:37 +0700

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -73,5 +73,7 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.userId);
     res.status(200).json("User has been deleted");
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
